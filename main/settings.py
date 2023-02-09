23785c62a0d841c14a4c1f2e28d102d52c299731
Paulo Coutinho <paulocoutinhox@gmail.com>
2023-02-09 20:27:24 -0300

    project updated

--- main/settings.py
+++ main/settings.py
@@ -171,8 +171,7 @@ TINYMCE_DEFAULT_CONFIG = {
     "width": "100%",
     "menubar": False,
     "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code fullscreen insertdatetime media table paste code help wordcount spellchecker",
-    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist checklist | forecolor "
-    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment code",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent | numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | insertfile image media pageembed template link anchor codesample | charmap emoticons | fullscreen preview save print | a11ycheck ltr rtl | showcomments addcomment code",
     "custom_undo_redo_levels": 10,
     "images_upload_url": "/upload_image/",
 }
