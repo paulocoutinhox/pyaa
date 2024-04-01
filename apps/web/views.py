c0ba8c969eca74e0d1fed3d528a598140b5ab3f0
“Paulo <paulo@ubook.com>
2024-04-01 02:11:38 -0300

    project updated

--- apps/web/views.py
+++ apps/web/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import UserDeleteForm
 
 
 def home_view(request):
     return render(request, "home/index.html")
+
+
+@login_required
+def account_profile_view(request):
+    return render(request, "account/account_profile.html")
+
+
+@login_required
+def account_delete_view(request):
+    if request.method == "POST":
+        form = UserDeleteForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            user = request.user
+            logout(request)
+            user.delete()
+            messages.info(request, "Your account has been deleted.")
+            return redirect("home")
+    else:
+        form = UserDeleteForm(instance=request.user)
+
+    context = {"form": form}
+
+    return render(request, "account/account_delete.html", context)
--- pyaa/settings.py
+++ pyaa/settings.py
@@ -19,7 +19,6 @@ from django.utils.translation import gettext_lazy as _
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -150,7 +149,6 @@ if ENV_PRODUCTION:
 else:
     CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -168,7 +166,6 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
