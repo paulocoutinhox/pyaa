27b4890b7cd86a5939ad82c5def8364973f1b66a
Paulo Coutinho <paulo@ubook.com>
2025-04-09 14:40:14 -0300

    project updated

--- pyaa/urls.py
+++ pyaa/urls.py
@@ -32,13 +32,37 @@ from apps.web.urls import urlpatterns as web_urlpatterns
 from . import views
 
 urlpatterns = [
-    path("api/", include("apps.customer.urls")),
-    path("api/", include("apps.language.urls")),
-    path("api/", include("apps.content.urls")),
-    path("api/", include("apps.gallery.urls")),
-    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
-    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/",
+        include("apps.customer.urls"),
+    ),
+    path(
+        "api/",
+        include("apps.language.urls"),
+    ),
+    path(
+        "api/",
+        include("apps.content.urls"),
+    ),
+    path(
+        "api/",
+        include("apps.gallery.urls"),
+    ),
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "api/schema/",
+        SpectacularAPIView.as_view(),
+        name="schema",
+    ),
     path(
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
@@ -49,11 +73,26 @@ urlpatterns = [
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
-    path("admin/", admin.site.urls),
-    path("upload-image/", views.upload_image),
-    path("tinymce/", include("tinymce.urls")),
-    path("i18n/", include("django_translation_flags.urls")),
-    re_path(r"^app/(?P<path>.*)$", views.serve_app_files),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
+    path(
+        "upload-image/",
+        views.upload_image,
+    ),
+    path(
+        "tinymce/",
+        include("tinymce.urls"),
+    ),
+    path(
+        "i18n/",
+        include("django_translation_flags.urls"),
+    ),
+    re_path(
+        r"^app/(?P<path>.*)$",
+        views.serve_app_files,
+    ),
 ]
 
 urlpatterns += web_urlpatterns
